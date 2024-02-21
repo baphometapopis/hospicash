c61f919ba55ad5e50a5d800a5f8bd963ecff8bb3
Joginder Yadav <joginder@indicosmic.com>
2024-02-21 11:55:47 +0530

    fixed bugs

--- src/components/dashboardcomponent/Modal/PaymentModal.js
+++ src/components/dashboardcomponent/Modal/PaymentModal.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useCallback, useEffect, useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
--- src/pages/Formpage/FormPage.jsx
+++ src/pages/Formpage/FormPage.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import React, { useCallback, useEffect, useState } from "react";
 import coverImage from "../../assets/img/hospicashcoverimage.jpeg";
 import { useFormik } from "formik";
